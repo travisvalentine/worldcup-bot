71298cdbaf473ab2db9a2862dee871bf0a1f4abb
Travis Valentine <tvalent2@gmualumni.org>
2014-06-10 21:57:03 -0400

    Reduce teams controller cruft

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,50 +1,14 @@
 class TeamsController < ApplicationController
-  # GET /teams
-  # GET /teams.json
   def index
-    @teams = Team.all
+    teams = Team.all
 
-    render json: @teams
+    render json: teams
   end
 
-  # GET /teams/1
-  # GET /teams/1.json
   def show
-    @team = Team.find(params[:id])
+    team = Team.where("short_name = ?", params[:id])
 
-    render json: @team
+    render json: team
   end
 
-  # POST /teams
-  # POST /teams.json
-  def create
-    @team = Team.new(params[:team])
-
-    if @team.save
-      render json: @team, status: :created, location: @team
-    else
-      render json: @team.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /teams/1
-  # PATCH/PUT /teams/1.json
-  def update
-    @team = Team.find(params[:id])
-
-    if @team.update(params[:team])
-      head :no_content
-    else
-      render json: @team.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /teams/1
-  # DELETE /teams/1.json
-  def destroy
-    @team = Team.find(params[:id])
-    @team.destroy
-
-    head :no_content
-  end
 end
