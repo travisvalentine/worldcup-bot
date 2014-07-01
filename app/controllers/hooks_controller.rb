e5446074b98761397528da8d11825293e4f9a0b4
Travis Valentine <tvalent2@gmualumni.org>
2014-07-01 11:04:34 -0400

    Add more on hooks

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -1,5 +1,12 @@
 class HooksController < ApplicationController
   def last_goal
-    campfire = Tinder::Campfire.new(ENV["cf_ssubdomain"], ENV["cf_stoken"])
+    data = params
+    campfire = Tinder::Campfire.new(ENV["cf_ssubdomain"], token: ENV["cf_stoken"])
+
+    room = campfire.find_room_by_id ENV["cf_sroom"]
+
+    p room
+    p room.name
+    p params.inspect
   end
 end
